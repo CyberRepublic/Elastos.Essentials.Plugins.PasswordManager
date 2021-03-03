8c92e8e87bacfeca2b9d069cd751b20b384a207d
raozhiming <raozhiming@elastos.org>
2021-03-03 11:38:20 +0800

    set light mode as default mode

--- src/android/UIStyling.java
+++ src/android/UIStyling.java
@@ -3,9 +3,10 @@ package org.elastos.essentials.plugins.passwordmanager;
 import android.graphics.Color;
 
 public class UIStyling {
-    public static int popupMainTextColor = Color.parseColor("#FFFFFF");
-    public static int popupInputHintTextColor = Color.parseColor("#CCCCCC");
-    public static int popupMainBackgroundColor = Color.parseColor("#FFFFFF");
+    // Default is light mode
+    public static int popupMainTextColor = Color.parseColor("#161740");
+    public static int popupInputHintTextColor = Color.parseColor("#161740");
+    public static int popupMainBackgroundColor = Color.parseColor("#F0F0F0");
     public static int popupSecondaryBackgroundColor = Color.parseColor("#FFFFFF");
 
     static void prepare(boolean useDarkMode) {
